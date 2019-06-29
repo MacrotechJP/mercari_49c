1ee62f0fbaf6b07a32f72932487537365702bee7
taka <tattaka.ta.dayo1234@gmail.com>
2019-06-29 17:28:39 +0900

    Basic-Authentication app;ication_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '49_mercari_wc' && password == '5555'
+    end
+  end
 end
