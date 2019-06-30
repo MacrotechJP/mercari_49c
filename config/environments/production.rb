2aaefa9c2629774af63a431b073963a86f13eb9e
taka <tattaka.ta.dayo1234@gmail.com>
2019-06-30 18:12:57 +0900

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-server '52.193.176.93', user: 'ec2-user', roles: %w{app db web}
