751cd33831caa9f9944cbbc7f4094d9fca950af1
millionstars <flashback169@yahoo.co.jp>
2019-07-06 19:26:51 +0900

    とりあえずヘッダーをコミット

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,29 @@
+$(function(){
+  // $('.product-form__input').on('keyup', function(e){
+  //   var input = $('.product-form__input').val();
+  //   console.log(input);
+  //   e.preventDefault();
+  //   var formData = new FormData(this);
+  // })
+  $('.pc-header-box-lower__left-parent-wrap').find('ul').hide();
+  $('.pc-header-box-lower__left li').hover(function(){
+  $(this).find('ul').show();
+  // console.log(input);
+  // e.preventDefault();
+  },
+  function(){  
+  $(this).find('ul').hide();
+  })
+
+  $('.pc-header-box-lower__left li').find('ul').hide();
+  $('.pc-header-box-lower__left li').hover(function(){
+  $(this).find('ul').show();
+  // console.log(input);
+  // e.preventDefault();
+  },
+  function(){
+  
+  $(this).find('ul li').hide();
+  })
+
+})
