2278735933654be18c9e0166f0ec7e2b14efa121
nitro <xiaoren8688@yahoo.co.jp>
2019-07-07 18:43:16 +0900

    update

--- app/assets/javascripts/topPage/header/header.js
+++ app/assets/javascripts/topPage/header/header.js
@@ -1,15 +1,7 @@
 $(function(){
-  // $('.product-form__input').on('keyup', function(e){
-  //   var input = $('.product-form__input').val();
-  //   console.log(input);
-  //   e.preventDefault();
-  //   var formData = new FormData(this);
-  // })
   $('.pc-header-box-lower__left-parent-wrap').find('ul').hide();
   $('.pc-header-box-lower__left li').hover(function(){
   $(this).find('ul').show();
-  // console.log(input);
-  // e.preventDefault();
   },
   function(){  
   $(this).find('ul').hide();
@@ -18,8 +10,6 @@ $(function(){
   $('.pc-header-box-lower__left li').find('ul').hide();
   $('.pc-header-box-lower__left li').hover(function(){
   $(this).find('ul').show();
-  // console.log(input);
-  // e.preventDefault();
   },
   function(){
   
