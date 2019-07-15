591bd6ab01d5108f52cd85ce59e18b85c7cbf14c
millionstars <flashback169@yahoo.co.jp>
2019-07-15 16:02:02 +0900

    商品詳細ページのクラスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'items#index'
+  # root 'items#index'
+  root 'items#show'
   resources :items, only: [:index]
 
 
