d3acdc0b47a0143de25296c6a5adce4a6cc32c8e
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-07-16 00:22:19 +0900

    Update

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,5 +1,6 @@
 $(function(){
 
+  //販売価格の計算を行う
   $("#price").on("keyup",function(){
     var price = $("#price").val();
 
@@ -11,37 +12,22 @@ $(function(){
       $("#rightDown").text("¥"+Math.round(price*0.9).toLocaleString());
     }
     
-    //console.log(price);
+  })
 
-
-
-
-
-
-
-
-
-
-
-
+  //配送についてのセレクトボックスを増表示する
+  $('#drive').change(function() {
+    var val = $('#drive').val();
+    console.log(111);
+    if(val != ""){
+      $(".driveWay").show();
+      $(".item_main_center-cell5").css('height', '+=110');
+      $(".item_main").css('height', '+=110');
+    }else{
+      $(".driveWay").hide();
+      $(".item_main_center-cell5").css('height', '-=110');
+      $(".item_main").css('height', '-=110');
+    }
+    
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
