ede0ded649516e33b3641fdd5b4669165b39f8ab
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-11 16:33:28 +0900

    Update

--- Gemfile
+++ Gemfile
@@ -83,3 +83,4 @@ group :development, :test do
   gem 'pry-doc'
 end
 gem 'ancestry'
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_152846) do
+ActiveRecord::Schema.define(version: 2019_08_11_072725) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 2019_08_10_152846) do
     t.string "deliveryWay"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "categories", "items"
