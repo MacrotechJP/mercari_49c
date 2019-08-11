6efb5766c8b70ef8412a72994e7ef67e22d1b30f
millionstars <flashback169@yahoo.co.jp>
2019-08-11 12:40:13 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
-  # root 'items#index'
+  root 'items#index'
   resources :items, only: [:index,:new,:create,:show]
   resources :users, only: [:show, :identification, :index]
   resources :profiles, only: [:show]
