3f9c327b981afd4f142c526621474158534c71c8
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-18 23:02:33 +0900

    ルーティング 修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,18 +20,10 @@ Rails.application.routes.draw do
   resources :identifications, only: [:show]
   resources :creditcards, only: [:show]
 
-  get "mypage/",to:"mypage#index"
-  get "mypage/profile",to:"mypage#profile"
-  get "mypage/identification",to:"mypage#identification"
-  resources :mypage do
-    member do
-      get 'index'
-      get 'signup2'
-      get 'signup3'
-      get 'signup4'
-      get 'signup5'
-      get 'signup6'
-      get 'login'
-    end
+  get 'mypage',to:'mypage#index'
+  namespace :mypage do
+    get 'profile'
+    get 'identification'
   end
+  
 end
