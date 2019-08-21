3056b89e647f28d4a9b9791e09d53fb13a4da1d4
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-21 21:57:11 +0900

    mypage_profileUpdate

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,7 +2,7 @@ class MypageController < ApplicationController
   before_action :require_sign
 
   def profile_update
-
+    User.find(current_user.id).update(nickname:update_params[:user_nickname],profile_description:update_params[:user_description])
     redirect_to mypage_profile_path
   end
 
@@ -11,6 +11,10 @@ class MypageController < ApplicationController
     unless user_signed_in?
       redirect_to root_path
     end
+  end
+  def update_params
+    attrs = [:user_nickname,:user_description]
+    params.permit(attrs)
   end
 
 end
