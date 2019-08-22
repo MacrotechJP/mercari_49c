434fa3e8586d0328ea2e3bdcc344ee752d04e63b
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-22 20:44:06 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
 
   has_many :images
+  has_many :categories
+  belongs_to :user, optional: true
   # belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   # belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
 end
