d7db8226b2dd1490813b9ce2500d3de70b4903f8
taka <tattaka.ta.dayo1234@gmail.com>
2019-08-24 17:54:42 +0900

    CustAddresss.save

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -17,18 +17,72 @@ class SignupController < ApplicationController
     session[:lastname] = user_params[:lastname]
     session[:firstname_kana] = user_params[:firstname_kana]
     session[:lastname_kana] = user_params[:lastname_kana]
-    session[:birthday] = user_params[:birthday]
+    session[:birthday] = user_params["birthday(1i)"]+"/"+user_params["birthday(2i)"]+"/"+user_params["birthday(3i)"]
     @user = User.new
   end
 
   def signup4
-    binding.pry
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
+    @address = CustAddresss.new
   end
 
   def signup5
+    @user = User.new
+    @address = CustAddresss.new
+    session[:firstname] =      address_params[:firstname]
+    session[:lastname] =       address_params[:lastname]
+    session[:firstname_kana] = address_params[:firstname_kana]
+    session[:lastname_kana] =  address_params[:lastname_kana]
+    session[:postal_code] =    address_params[:postal_code]
+    session[:prefecture] =     address_params[:prefecture]
+    session[:municipality] =   address_params[:municipality]
+    session[:house_number] =   address_params[:house_number]
+    session[:building_name] =  address_params[:building_name]
+    session[:phone_number] =   address_params[:phone_number]
   end
 
   def signup6
+    @address = CustAddresss.new
+    @user = User.new
+  end
+
+  def signup7
+  end
+
+  def create
+    @user = User.new(
+      nickname: session[:nickname],
+      email:    session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      firstname:session[:firstname],
+      lastname_kana: session[:lastname],
+      firstname_kana: session[:firstname_kana],
+      lastname_kana: session[:lastname_kana],
+      birthday: session[:birthday],
+      phone_number: session[:phone_number]
+    )
+    @address = CustAddresss.new(
+      firstname:session[:firstname],
+      lastname:session[:lastname],
+      firstname_kana:session[:firstname_kana],
+      lastname_kana:session[:lastname_kana],
+      postal_code:session[:postal_code],
+      prefecture:session[:prefecture],
+      municipality:session[:municipality],
+      house_number:session[:house_number],
+      building_name:session[:building_name],
+      phone_number:session[:phone_number]
+    )
+    if @user.save
+      @address.save
+      binding.pry
+      sign_in @user
+      redirect_to signup7_signup_index_path
+    else
+      redirect_to signup7_signup_index_path
+    end
   end
 
   private
@@ -42,7 +96,25 @@ class SignupController < ApplicationController
       :lastname,
       :firstname_kana,
       :lastname_kana,
-      :birthday
+      "birthday(1i)",
+      "birthday(2i)",
+      "birthday(3i)",
+      :phone_number
+    )
+  end
+
+  def address_params
+    params.permit(
+      :firstname,
+      :lastname,
+      :firstname_kana,
+      :lastname_kana,
+      :postal_code,
+      :prefecture,
+      :municipality,
+      :house_number,
+      :building_name,
+      :phone_number
     )
   end
 end
