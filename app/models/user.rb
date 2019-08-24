d6c009e11aba723eb49c5e836834160b2fbf74e7
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-24 20:39:19 +0900

    admin修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :comments
-  has_many :rates
-  has_many :creditcards
-  has_many :likes
-  has_many :cust_addresses
-  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
-  has_many :selling_items, -> { where( "buyer_id is NULL" ) }, foreign_key: "seller_id", class_name: "Item"
-  has_many :sold_items, -> { where( "buyer_id is no NULL" ) }, foreign_key: "seller_id", class_name: "Item"
+#   has_many :comments
+#   has_many :rates
+#   has_many :creditcards
+#   has_many :likes
+#   has_many :cust_addresses
+#   has_many :items
+#   has_many :selling_items, -> { where( "buyer_id is NULL" ) }, foreign_key: "seller_id", class_name: "Item"
+#   has_many :sold_items, -> { where( "buyer_id is no NULL" ) }, foreign_key: "seller_id", class_name: "Item"
 
 end
