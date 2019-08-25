97cda6b46314686e2a6c6471f8d3658c9adca92f
nitro <xiaoren8688@yahoo.co.jp>
2019-08-25 19:03:47 +0900

    Revert "Revert "Revert "Merge branch 'server-side.newly.registration'"""
    This reverts commit 8befa6c1e2a30db0fa0145dd951d711151843ce4.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-    keys: [:email, :password, :password_confirmation, :firstname,:lastname, :firstname_kana, :lastname_kana, :birthday, :phone_number])
-  end
-
 
   def production?
     Rails.env.production?
