7a3a85fd997e846cb41dd4ee2d8d378107b169a4
taka <tattaka.ta.dayo1234@gmail.com>
2019-08-30 19:53:08 +0900

    routing for callback

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'purchase/done'
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'items#index'
