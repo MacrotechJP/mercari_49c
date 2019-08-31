9a7645d8ea2ebfd9f3d85648cb9757a2f8bd4762
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-31 15:05:33 +0900

    up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
 
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
+  
   has_many :items
   has_many :comments
   has_many :rates
@@ -19,5 +32,9 @@ class User < ApplicationRecord
   has_many :cust_addressses
   accepts_nested_attributes_for :cust_addressses
 
+  private
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
 
